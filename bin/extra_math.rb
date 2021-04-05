fad06fa1b64ce9f9455fc497594ac6bcf8ca0a73
Rawezh <rawezhmarif@gmail.com>
2021-04-05 11:07:47 +0300

    added factorial function

--- bin/extra_math.rb
+++ bin/extra_math.rb
@@ -1,7 +1,15 @@
 def cos(a)
     sum = 0
     for i in 0..100 do
-        sum = sum + (a**2*i)/fact(2*i)
+        sum += (a**2*i)/fact(2*i)
     end
     sum
+end
+
+def fact(n)
+    result = 1
+    for i in 0..n do
+        result *= i
+    end
+    result
 end
